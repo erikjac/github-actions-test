d320d41a2783d39f438f5cefa8560aef752f9da5
Eric Jakobsson <eric.jakobsson@getway.app>
2020-10-02 12:56:31 +0200

    env-file action

---
+++ .github/actions/env-file/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+import re
+
+if len(sys.argv) == 1:
+    sys.exit("Missing input")
+
+envs = sys.argv[1]
+
+def parse_escaped_chars(env):
+    return re.sub(r"\\(\"|,)",r"\1", env) 
+    #return env.replace(r"\",r"")
+
+def parse_input(envs):
+    return re.split(r"(?<!\\),", envs)
+    #return envs.split(",")
+
+def gen_output(envs):
+    return "\n".join(envs)
+
+output = gen_output([parse_escaped_chars(env) for env in parse_input(envs)])
+print(output)
+
